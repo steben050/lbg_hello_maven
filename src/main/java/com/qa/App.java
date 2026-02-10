b61e7db89643f4f9df44a3df278dcccdfc1ff075
steben050 <steben050@outlook.com>
2026-02-10 15:06:58 +0000

    EDIT: changed snapshot version and added new line of code

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,8 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Steve"));
+        System.out.println(sayHelloToSomeone("Victor"));
         System.out.println(sayGoodbye());
     }
 
